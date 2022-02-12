f94e52cf5883c0da44cef36550d14a1d66f7c5e5
ryang223 <ryangoodwd@gmail.com>
2022-02-12 10:41:15 -0500

    ES6 examples finished

---
+++ JS_boilerplate/JS_boilerplate(rawjs)/ES6.js
@@ -0,0 +1,16 @@
+//Function with no parameters
+const func = () => {/*function code here*/}
+
+// Function with one parameter
+const func = (parameter => {/*function code here*/}
+
+//Function with multiple parameters 
+const func = (parameter1, parameter2) => {/*function code here*/}
+
+//Function with a single expression
+const func = () => //expression
+
+//Multi-line Function 
+const func = () => {
+    //Function code here 
+}
